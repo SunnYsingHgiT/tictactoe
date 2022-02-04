7d9374b6b49d03dcc8d6479a7ea3e3b8fd635ffd
Sunny <im.singhsunny053@gmail.com>
2022-02-04 20:07:56 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get minimal than this React app.</p>
+      <small>hello this is me your developer</small>
+    </>
+  );
+};
+
+export default App;
