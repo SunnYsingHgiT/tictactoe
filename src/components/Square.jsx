9915103ed72b8425731062a2776bd71e2894153c
Sunny <im.singhsunny053@gmail.com>
2022-02-06 19:45:14 +0530

    Calculate winner and lift up the state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const square = ({ value, onClick }) => {
+  console.log('square rerender');
   return (
     <button type="button" className="square" onClick={onClick}>
       {value}
